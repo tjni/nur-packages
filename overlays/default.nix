9a0bedd4b9cff9e8a6ce633dea2b30cfd42b73cb
Theodore Ni <3806110+tjni@users.noreply.github.com>
2022-04-25 15:51:43 -0500

    Add overlay for a custom, forked version of pueue.

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,4 +4,10 @@
   #   https://github.com/caddyserver/caddy
   #
   caddy-current = import ./caddy-current;
+
+  # My forked version of pueue with changes that have not yet been upstreamed.
+  #
+  #   https://github.com/tjni/pueue 
+  #
+  tjni-pueue = import ./tjni-pueue;
 }
---
+++ overlays/tjni-pueue/default.nix
@@ -0,0 +1,25 @@
+final: prev: {
+
+  pueue = let
+    rev = "84c3efd85e384884cc673d5bf9425208470a531e";
+    pname = "pueue";
+    version = "2.0.2-${builtins.substring 0 11 rev}";
+
+    src = prev.fetchFromGitHub {
+      owner = "tjni";
+      repo = pname;
+      inherit rev;
+      sha256 = "sha256-f3Kb91v1uvgjI82BSerRxkBqivBmBPKxoKYbPyl88hY=";
+    };
+
+    overriddenRustPlatform = prev.rustPlatform // {
+      buildRustPackage = args: prev.rustPlatform.buildRustPackage (args // {
+        inherit pname version src;
+        cargoSha256 = "sha256-b5jGTu55mDw2I7ut8doD7F6iM4R8K7X9l8/vUxZ30sc=";
+      });
+    };
+  in
+    prev.pueue.override {
+      rustPlatform = overriddenRustPlatform;
+    };
+}
