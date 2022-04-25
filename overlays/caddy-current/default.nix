2db19166ae28b708984849510bbd9fd68c92834d
Theodore Ni <3806110+tjni@users.noreply.github.com>
2022-04-25 17:42:02 -0500

    Update version to just released 2.5.0.

--- overlays/caddy-current/default.nix
+++ overlays/caddy-current/default.nix
@@ -1,7 +1,7 @@
 final: prev: {
   caddy = let
     rev = "a8bb4a665af358f61a7ac0eabac8df2110cb6a36";
-    version = "2.5.0-rc.1-${builtins.substring 0 11 rev}";
+    version = "2.5.0-${builtins.substring 0 11 rev}";
 
     src = prev.fetchFromGitHub {
       owner = "caddyserver";
